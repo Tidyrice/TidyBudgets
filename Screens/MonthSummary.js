7caff47c5228c07fb24487031e3a0185b690b138
Cheney Cai <cheneycai2004@hotmail.com>
2022-12-28 03:27:50 -0500

    UI for month summary screen

--- Screens/MonthSummary.js
+++ Screens/MonthSummary.js
@@ -1,39 +1,5 @@
 import { StyleSheet, Text, View, SafeAreaView, Button, FlatList, StatusBar, TouchableOpacity } from 'react-native';
 
-const DATA = [
-    {
-      title: "Nov. 28",
-      price: "$5.00"
-    },
-    {
-      title: "Nov. 21",
-      price: "$5.00"
-    },
-    {
-      title: "Nov. 20",
-      price: "$5.00"
-    },
-    {
-        title: "Nov. 17",
-        price: "$5.00"
-    },
-    {
-        title: "Nov. 13",
-        price: "$5.00"
-    },
-    {
-        title: "Nov. 1",
-        price: "$5.00"
-    },
-];
-
-const renderItem = ({ item }) => (
-    <View style={styles.listItem}>
-        <Text style={styles.listTitle}>{item.title}</Text>
-        <Text>{item.price}</Text>
-    </View>
-);
-
 //takes in parameter month (string) and spending (array of spending objects)
 export function MonthSummary( {navigation} ) {
 
@@ -48,9 +14,11 @@ export function MonthSummary( {navigation} ) {
 
     return(
 
-        <SafeAreaView style={ styles.container }>
+        <SafeAreaView style = { styles.container }>
 
-            <Text style={ styles.title }>Total Spending: $169,420.69</Text>
+            <View style = { styles.titleContainer }>
+                <Text style = { styles.title }>Total Spending: $420.69</Text>
+            </View>
 
             <FlatList
                 data = {DATA}
@@ -71,26 +39,106 @@ export function AddSpendingScreen() {
     )
 }
 
+const DATA = [ //SILLY DATA FOR DEBUGGING PURPOSES
+    {
+        title: "Nov. 28",
+        type: "Food",
+        cost: "$5.00",
+        location: "Lazzez",
+    },
+    {
+        title: "Nov. 21",
+        type: "Transportation",
+        cost: "$270.00",
+        location: "Edmonton Public Libraries",
+    },
+    {
+        title: "Nov. 20",
+        type: "--",
+        cost: "$5.00",
+        location: "Lazeez",
+    },
+    {
+        title: "Nov. 17",
+        type: "Food",
+        cost: "$5.00",
+        location: "Lazeez",
+    },
+    {
+        title: "Nov. 13",
+        type: "Food",
+        cost: "$5.00",
+        location: "Lazeez",
+    },
+    {
+        title: "Nov. 1",
+        type: "Food",
+        cost: "$5.00",
+        location: "Lazeez",
+    },
+];
+
+const renderItem = ({ item }) => (
+    <View style = { styles.listItem }>
+
+        <View style = {{ flex: -1 }}>
+            <Text style = { styles.itemDate }>{ item.title }</Text>
+            <Text style = { styles.itemLocation }>{ item.location }</Text>
+            <Text style = { styles.itemType }>{ item.type }</Text>
+        </View>
+
+        <View>
+            <Text style = { styles.itemCost }>{ item.cost }</Text>
+        </View>
+
+    </View>
+);
+
 const styles = StyleSheet.create({
 
     container: {
         flex: 1,
         justifyContent: "center",
     },
-    title: {
+    titleContainer: {
         padding: 20,
-        marginVertical: 8,
+        backgroundColor: "white",
+        shadowColor: "black",
+        elevation: 10,
+    },
+    title: {
+        paddingVertical: 4,
         fontSize: 28,
         fontWeight: "bold",
     },
-    listItem: {
-        backgroundColor: "#f9c2ff",
+    listItem: { //card for each list element
+        backgroundColor: "#BFDFFF",
+        borderColor: "gray",
+        borderWidth: 1,
+        borderRadius: 4,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
         padding: 20,
-        marginVertical: 8,
+        marginTop: 10,
         marginHorizontal: 12,
     },
-    listTitle: {
-        fontSize: 26,
+    itemDate: {
+        paddingBottom: 8,
+        fontSize: 24,
+    },
+    itemLocation: {
+        paddingBottom: 8,
+        fontSize: 18,
+    },
+    itemType: {
+        fontStyle: "italic",
+        fontSize: 18,
+    },
+    itemCost: {
+        alignSelf: "flex-end",
+        paddingLeft: 8,
+        fontSize: 28,
     },
 
 });
