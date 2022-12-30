ed7a72f37ab47557f9e66f112a9103caa7b979bb
Cheney Cai <cheneycai2004@hotmail.com>
2022-12-30 02:29:45 -0500

    saveMonth

--- Data Management/SaveSystem.js
+++ Data Management/SaveSystem.js
@@ -2,12 +2,14 @@ import * as fs from "react-native-fs";
 import { data } from "../App";
 
 //functions to manage how to save/load data from the local database
-export function Save() {
+export function SaveMonth(year, month) {
 
+    const path = `${fs.DocumentDirectoryPath}/${year}-${month}.json`
+    
 }
 
 export function LoadMonth(year, month) {
 
     const path = `${fs.DocumentDirectoryPath}/${year}-${month}.json`
-    
+
 }
