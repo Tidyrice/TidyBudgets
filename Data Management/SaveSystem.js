1d5241db0040b92b7da4619cc3172eab84c6c0b2
Cheney Cai <cheneycai2004@hotmail.com>
2022-12-30 02:28:57 -0500

    fs init

--- Data Management/SaveSystem.js
+++ Data Management/SaveSystem.js
@@ -1,3 +1,4 @@
+import * as fs from "react-native-fs";
 import { data } from "../App";
 
 //functions to manage how to save/load data from the local database
@@ -5,6 +6,8 @@ export function Save() {
 
 }
 
-export function Load() {
+export function LoadMonth(year, month) {
 
+    const path = `${fs.DocumentDirectoryPath}/${year}-${month}.json`
+    
 }
